b9bbde66563210d5f31a22c05c97fe1d081f92e6
qiray <zotovyaa@mail.ru>
2020-01-22 17:50:17 +0300

    translations started

--- translations.py
+++ translations.py
@@ -11,11 +11,22 @@ data = {
 default_lang = "en"
 
 class Translation():
+
+    def __new__(cls, lang=default_lang):
+        """Singleton"""
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(Translation, cls).__new__(cls)
+        return cls.instance
+
     def __init__(self, lang=default_lang):
+        self.set_locale(lang)
+
+    def set_locale(self, lang):
         if not lang in data:
             lang = default_lang
         self.lang = lang
         self.data = data[lang]
+
     def get(self, key):
         if not key in self.data:
             return key
